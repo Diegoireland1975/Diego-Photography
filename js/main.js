16ff1eab4c6790f6547b2093fc21d3b63dc1f394
unknown <diego170975@gmail.com>
2020-12-16 14:42:02 +0000

    add images on scroll

--- js/main.js
+++ js/main.js
@@ -6,4 +6,23 @@ $('[data-fancybox="gallery"]').fancybox({
   buttons: ["zoom", "slideShow", "fullScreen", "thumbs", "close"],
 });
 
-// ####lazy loading images####
+// ####lazy loading images on scroll####
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = window.innerHeight * 0.6;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
