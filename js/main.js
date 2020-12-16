c949e6d290ab3bb18efc698dc157e1d9ba4a9bc2
unknown <diego170975@gmail.com>
2020-12-16 23:00:15 +0000

    add images on scroll

--- js/main.js
+++ js/main.js
@@ -6,16 +6,24 @@ $('[data-fancybox="gallery"]').fancybox({
   buttons: ["zoom", "slideShow", "fullScreen", "thumbs", "close"],
 });
 
-// ####lazy loading images on scroll####
-const boxes = document.querySelectorAll(".box");
+// #################################
+// ####loading images on scroll####
+// #################################
 
+// getting all the boxes class
+const boxes = document.querySelectorAll(".box");
+// getting all the caption class
+const captions = document.querySelectorAll(".caption");
+// add even listener on scroll
 window.addEventListener("scroll", checkBoxes);
+window.addEventListener("scroll", checkCaptions);
 
 checkBoxes();
 
 function checkBoxes() {
-  const triggerBottom = window.innerHeight * 0.6;
+  const triggerBottom = (window.innerHeight / 5) * 4;
 
+  // for each loop for the box class
   boxes.forEach((box) => {
     const boxTop = box.getBoundingClientRect().top;
 
