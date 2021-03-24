e21a28d9a7a8ce99478919186d6d450b3fa0d1a3
Diego Raffa <diego170975@gmail.com>
2021-03-24 20:32:36 +0000

    added media q for bigger screen

--- js/main.js
+++ js/main.js
@@ -10,27 +10,27 @@ $('[data-fancybox="gallery"]').fancybox({
 // ####loading images on scroll####
 // #################################
 
-// getting all the boxes class
-const boxes = document.querySelectorAll(".box");
-// getting all the caption class
-const captions = document.querySelectorAll(".caption");
-// add even listener on scroll
-window.addEventListener("scroll", checkBoxes);
-window.addEventListener("scroll", checkCaptions);
+// // getting all the boxes class
+// const boxes = document.querySelectorAll(".box");
+// // getting all the caption class
+// const captions = document.querySelectorAll(".caption");
+// // add even listener on scroll
+// window.addEventListener("scroll", checkBoxes);
+// window.addEventListener("scroll", checkCaptions);
 
-checkBoxes();
+// checkBoxes();
 
-function checkBoxes() {
-  const triggerBottom = (window.innerHeight / 5) * 4;
+// function checkBoxes() {
+//   const triggerBottom = (window.innerHeight / 5) * 4;
 
-  // for each loop for the box class
-  boxes.forEach((box) => {
-    const boxTop = box.getBoundingClientRect().top;
+//   // for each loop for the box class
+//   boxes.forEach((box) => {
+//     const boxTop = box.getBoundingClientRect().top;
 
-    if (boxTop < triggerBottom) {
-      box.classList.add("show");
-    } else {
-      box.classList.remove("show");
-    }
-  });
-}
+//     if (boxTop < triggerBottom) {
+//       box.classList.add("show");
+//     } else {
+//       box.classList.remove("show");
+//     }
+//   });
+// }
